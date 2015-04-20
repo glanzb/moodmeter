ef7e7f2260dca9ff33a1ecce1c13c606c1b5ac9f
Peter Rockwood <peter.rockwood@gmail.com>
2015-04-20 14:18:39 -0700

    push for merge with master

--- nodeTweetStream/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	twtConsumer_key: "jmIG6E2dnbcGkPHr3FGWK6Lvd",
-    twtConsumer_secret: "v2SwwluRI6WQiLercG1MLYqUNRS9dPhoymKIXfcYuE65oqQP5h",
-    twtToken: "3143092806-oXvalEaNqnR7Yuanhfv9ROfUGJf06E4dAeuNsCl",
-    twtToken_secret: "4Uc5wKaok73HTqZOSs6WCjeNosFWDBQhUUK7KMRfC7MWy"
-}
