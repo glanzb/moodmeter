997c8b88834c60ead88ab84d2e52319cd660d703
Peter Rockwood <peter.rockwood@gmail.com>
2015-04-22 20:53:18 -0700

    hooked-up server to testIndex, poorly

--- testFiles/testIndex.js
+++ testFiles/testIndex.js
@@ -1,3 +1,5 @@
+var pattern = null;
+
 $(function(){
 
 	var socket = io();
@@ -6,21 +8,48 @@ $(function(){
       //   $('#m').val('');
       //   return false;
       // });
-	
-	var pattern = Trianglify({
-  	height: 300,
- 	width: 300,
-  	cell_size: 40});
 
-	document.body.appendChild(pattern.canvas());
+	var colorFunc = function(x, y) {
+    		return 'hsl('+Math.floor(Math.abs(x*y)*360)+',70%,60%)';
+	};
+	
+	// pattern = Trianglify({
+	// 	height: 300,
+	// 	width: 300,
+	// 	cell_size: 40,
+	// 	color_function: colorFunc
+	// });
+
+	// $("body").append(pattern.svg());
 
 	socket.on('data', function(wordData){
-  //
-  		//console.log(wordData);
-  		$('#data').text("");
-  		$('#data').text(JSON.stringify(wordData));
-  		pattern.
-	});
 
+		if(wordData.total % 10 === 0){
+			//console.log(wordData);
+			//$('#data').text("");
+			//$('#data').text(JSON.stringify(wordData));
+			var hFreq = wordData.wordsFreq['happy'];
+			pattern = Trianglify({
+				height: 300,
+				width: 300,
+				cell_size: 10,
+				seed: 'gn26p',
+				color_function: function(x, y) {
+								return 'hsl(' + Math.floor(Math.abs(x*y)*(hFreq*100)) + ',70%,60%)';
+								}
+			});
+
+			
+			//console.log(hFreq);
+			// pattern.cell_size = Math.floor(Math.random()*100);
+			//console.log('pattern.cell_size: '+ pattern.cell_size);
+			pattern.color_function = function(x, y) {
+			return 'hsl(' + Math.floor(Math.abs(x*y)*(hFreq*20)) + 
+				',70%,60%)';
+			};
+			$("svg").remove();
+			$("body").append(pattern.svg());
+		}
+  	});
 
 });
