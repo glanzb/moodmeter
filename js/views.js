c9155a993bf65ff8a876852f1334269d7d02cc1d
Thsn <theisen83@yahoo.com>
2015-04-24 18:34:48 -0700

    updated views.js - commented out early trianglify - moved current trianglify to the canvas view

--- js/views.js
+++ js/views.js
@@ -1,5 +1,5 @@
 // --- Models ---
-var TriPictures = (function() {
+/*var TriPictures = (function() {
 	var TriPic = Backbone.Model.extend({
 	});
 
@@ -75,7 +75,7 @@ var data = new Data({
 		  //drawBar(twitCount);
 		});
 	})
-});
+});*/
 
 
 // --- Views ---
@@ -110,28 +110,116 @@ var DataView = Backbone.View.extend({
 });
 
 var CanvasView = Backbone.View.extend({
-	tagName: 'canvas',
+	tagName: 'div',
+	
+
+
+		triangles: function(){
+		var socket = io();
+		var wordData;
+
+		$(function(){	
+			socket.on('data', function(wordData){
+				$('#data').text("");
+				$('#data').text(JSON.stringify(wordData.tweet));			
+		  	});
+		});
+
+		$(function(){
+			var hFreq = [1, 1];
+			var gFreq = [1, 1];
+
+			socket.on('timedData', function(wordData){
+				//console.log(hFreq);
+				hFreq.pop();
+				hFreq.unshift(wordData.wordsFreqProportions['happy_sad']);
+				gFreq.pop();
+				gFreq.unshift(wordData.wordsFreqProportions['good_bad']);
+
+				var i = 0;
+
+				interval(function(){
+					var xShift = lerp(hFreq[1], hFreq[0], i);
+					var yShift = lerp(gFreq[1], gFreq[0], i);
+					i++;
+					pattern = Trianglify({
+						height: 600,
+						width: 1000,
+						variance: .5 + ((Math.random()-0.5)/10),
+						cell_size: 100,
+						seed: 'gn26p',
+						color_function: function(x, y) {
+							//console.log(y)
+							//return 'hsl(' + Math.floor((x*50)+(xShift*10)) + ','+ Math.floor(x/20) +'%,60%)'
+							return 'hsl(' + Math.floor((x*50)+(xShift*200)) + ',' + Math.floor((y)*(yShift*500)) + '%,'+ (40+(y*60)) + '%)'
+						}
+						
+					});
+					//console.log(xShift);
+					$("canvas").remove();
+					$("#picture").append(pattern.canvas());
+					
+				}, 50, 20);
+			});
+
+
+
+			function lerp(a,b,t){
+				//console.log(t)
+				var interp = a + ((t * .15) * (b - a));
+				return interp
+			};
+
+			function interval(func, wait, times){
+			    var interv = function(w, t){
+			        return function(){
+			            if(typeof t === "undefined" || t-- > 0){
+			                setTimeout(interv, w);
+			                try{
+			                    func.call(null);
+			                }
+			                catch(e){
+			                    t = 0;
+			                    throw e.toString();
+			                }
+			            }
+			        };
+			    }(wait, times);
+
+			    setTimeout(interv, wait);
+			};
+
+		});
+
+			},
 
 	// events: {'click': 'click'},
 	initialize: function(opts){
-		console.log(trianglify.pattern);
-		console.log(this.el);
+		this.triangles();
+		this.render();
+		this.$el.appendTo('#picture');
+
 		//this.n = opts.n;		
 		//document.getElementById('main').appendChild(trianglify.pattern.canvas(this.el));
-		trianglify.pattern.canvas(this.el);
+		
+		//trianglify.pattern.canvas(this.el);
 	},
-	render: function(){
-	}	
-	// click: function(evt) {
-	// 	this.collection.refresh(this.n);
-	// }
+	render: function(){		
+			
+		return this;
+	},	
+
+
+			// click: function(evt) {
+			// 	this.collection.refresh(this.n);
+			// }
 });
 
 // var 
 
 var GalleryView = Backbone.View.extend({
 	tagName: 'section',
-	model:TriPictures,
+	//model:TriPictures,
 	template: template.gallery,
 	// events: {'click': 'click'},
 	initialize: function(opts){
@@ -188,8 +276,9 @@ var MainView = Backbone.View.extend({
 		};
 
 		// makeSubView('nav',NavView);
-		makeSubView('data',DataView);
+		
 		makeSubView('picture',CanvasView);
+		makeSubView('data',DataView);
 		makeSubView('gallery',GalleryView);
 		makeSubView('about', AboutView);
 		
@@ -205,8 +294,8 @@ var MainView = Backbone.View.extend({
 
 var page = null, trianglify = null;
 function makePage() {
-	trianglify = new TriPictures();
-	page = new MainView({collection:trianglify});
+	//trianglify = new TriPictures();
+	page = new MainView(); //MainView({collection:trianglify});
 	
 	page.render();
 }
