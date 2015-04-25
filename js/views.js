b0172f8ebd85298365c817d29bc639f89598b5f8
Peter Rockwood <peter.rockwood@gmail.com>
2015-04-24 17:06:27 -0700

    put trianglify animation in views.js

--- js/views.js
+++ js/views.js
@@ -77,25 +77,6 @@ var data = new Data({
 	})
 });
 
-
-// --- Views ---
-// var NavView = Backbone.View.extend({
-// 	tagName: 'div',
-// 	template: template.nav,
-// 	// events: {'click': 'click'},
-// 	initialize: function(opts){
-// 		this.n = opts.n;
-// 		this.$el.html(template.nav);
-// 		//this.$el.appendTo(opts.$div); 
-// 	},
-// 	render: function(){
-
-// 	},
-// 	// click: function(evt) {
-// 	// 	this.collection.refresh(this.n);
-// 	// }
-// });
-
 var DataView = Backbone.View.extend({
 	tagName: 'div',
 	template: '',
@@ -110,22 +91,83 @@ var DataView = Backbone.View.extend({
 });
 
 var CanvasView = Backbone.View.extend({
-	tagName: 'canvas',
+	tagName: 'div id="trify"',
 
 	// events: {'click': 'click'},
 	initialize: function(opts){
 		console.log(trianglify.pattern);
 		console.log(this.el);
-		//this.n = opts.n;		
-		//document.getElementById('main').appendChild(trianglify.pattern.canvas(this.el));
-		trianglify.pattern.canvas(this.el);
-	},
-	render: function(){
-	}	
-	// click: function(evt) {
-	// 	this.collection.refresh(this.n);
-	// }
-});
+
+		var socket = io();
+		var hFreq = [1, 1];
+		var gFreq = [1, 1];
+
+		socket.on('timedData', function(wordData){
+			//console.log(hFreq);
+			hFreq.pop();
+			hFreq.unshift(wordData.wordsFreqProportions['happy_sad']);
+			gFreq.pop();
+			gFreq.unshift(wordData.wordsFreqProportions['good_bad']);
+
+			var i = 0;
+
+			interval(function(){
+				var xShift = lerp(hFreq[1], hFreq[0], i);
+				var yShift = lerp(gFreq[1], gFreq[0], i);
+				i++;
+				pattern = Trianglify({
+					height: 600,
+					width: 1000,
+					variance: .5 + ((Math.random()-0.5)/10),
+					cell_size: 100,
+					seed: 'gn26p',
+					color_function: function(x, y) {
+						//console.log(y)
+						//return 'hsl(' + Math.floor((x*50)+(xShift*10)) + ','+ Math.floor(x/20) +'%,60%)'
+						return 'hsl(' + Math.floor((x*50)+(xShift*200)) + ',' + Math.floor((y)*(yShift*500)) + '%,'+ (40+(y*60)) + '%)'
+					}
+				});
+				//console.log(xShift);
+				$("canvas").remove();
+				$("#picture").append(pattern.canvas());
+				
+			}, 50, 20);
+		});
+
+
+
+		function lerp(a,b,t){
+			//console.log(t)
+			var interp = a + ((t * .15) * (b - a));
+			return interp
+		};
+
+		function interval(func, wait, times){
+		    var interv = function(w, t){
+		        return function(){
+		            if(typeof t === "undefined" || t-- > 0){
+		                setTimeout(interv, w);
+		                try{
+		                    func.call(null);
+		                }
+		                catch(e){
+		                    t = 0;
+		                    throw e.toString();
+		                }
+		            }
+		        };
+		    }(wait, times);
+
+		    setTimeout(interv, wait);
+		};
+			
+		},
+		render: function(){
+		}	
+		// click: function(evt) {
+		// 	this.collection.refresh(this.n);
+		// }
+		});
 
 // var 
 
@@ -188,8 +230,9 @@ var MainView = Backbone.View.extend({
 		};
 
 		// makeSubView('nav',NavView);
-		makeSubView('data',DataView);
+		
 		makeSubView('picture',CanvasView);
+		makeSubView('data',DataView);
 		makeSubView('gallery',GalleryView);
 		makeSubView('about', AboutView);
 		
