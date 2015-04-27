2fc1940ae4060639814b686b8365283c783587a1
Peter Rockwood <peter.rockwood@gmail.com>
2015-04-26 17:52:21 -0700

    slowed dem tweets down

--- psyServer.js
+++ psyServer.js
@@ -75,8 +75,10 @@ tw.on('tweet', function(tweet){
       wordData.time = Date.now();
       wordData.tweet = tweet.text;
       wordData.biggest = biggest();
-
-      io.emit('data', wordData);
+      if (wordData.total%100 === 0){
+        io.emit('data', wordData);
+      }
+      
     }
   })
 
@@ -119,7 +121,9 @@ setInterval(function(){
   console.log(current_wordData.time.toString());
   db.put(dbCollectionName, current_wordData.time.toString(), current_wordData, false)
   .then(function(res){console.log('one datum posted to db. datum id:  '+ current_wordData.time.toString())})
-  .fail(function(error){console.log('db post failed: '+error.body)});
+  .fail(function(error){console.log('db post failed: '+error.
+
+    body)});
 }, 500000);
 
 
@@ -163,10 +167,10 @@ app.get('/test/api', function(req,res){
     .collection(dbCollectionName)
     .limit(100)
     .sort('key','asc')
-    .query(searchStr)  key:[dateToMs(April 24, 2015) TO dateToMs(April 25, 2015) ]
+    .query(searchStr)  //key:[dateToMs(April 24, 2015) TO dateToMs(April 25, 2015) ]
     .then(forwardOrchResults) // makeChart
     .fail(handleFailure)
-}
+
 
 });
 
